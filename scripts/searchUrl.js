2a9c9f60137a7295d7e79b181c3d6bdb478527cd
Herokine <herokine23@gmail.com>
2024-12-17 20:38:00 +0800

    feat: Add search URL script

---
+++ scripts/searchUrl.js
@@ -0,0 +1,19 @@
+const params = {
+  id: '',
+  token: '',
+  query: key,
+  p: page,
+};
+const searchPath = `/search/?query=${java.encodeURI(params.query)}&p=${params.p}`;
+const searchHTML = java.ajax(`https://hlib.cc${searchPath}`);
+
+params.id = String(searchHTML).match(/id:\s'([a-zA-Z-_\d]+)',/)[1];
+params.token = java.md5Encode(`${params.id}search${searchPath}`);
+
+`https://hlib.cc/api/search/,${JSON.stringify({
+  method: 'POST',
+  body: JSON.stringify(params),
+  headers: {
+    'content-type': 'application/json',
+  },
+})}`;
