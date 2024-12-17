a10d2d8208e84d67a40652632938283b3c404d94
Herokine <herokine23@gmail.com>
2024-12-17 23:00:47 +0800

    feat(content): Add next content rule

---
+++ source/rules/Content/nextContentUrl.js
@@ -0,0 +1,15 @@
+((html) => {
+  const htmlParsed = org.jsoup.Jsoup.parse(html);
+  const novelIdMatch = html.match(/fetch\('\/comment\/n\/(\d+)'/);
+  const nextPageBtn = htmlParsed.select('#content nav .btn-group > button[onclick]:last-child');
+  if (!novelIdMatch || !nextPageBtn) return '';
+
+  const nextPageFn = String(nextPageBtn.attr('onclick'));
+  if (!nextPageFn) return '';
+
+  const novelId = novelIdMatch[1];
+  const nextPageFnParams = nextPageFn.match(/rp\('([a-zA-Z-_\d]+)',\s(\d+)\);/);
+  if (!novelId || !nextPageFnParams) return '';
+
+  return `https://hlib.cc/n/${novelId}?${nextPageFnParams[1]}=${nextPageFnParams[2]}`;
+})(result);
