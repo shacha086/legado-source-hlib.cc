c8b59e0a4db0193af8c2def3a7102f51f2343030
Herokine <herokine23@gmail.com>
2024-12-19 01:35:11 +0800

    feat(script/build): Add UglifyJS back

--- script/build.js
+++ script/build.js
@@ -5,6 +5,12 @@ import JSON5 from 'json5';
 import UglifyJS from 'uglify-js';
 
 const RegExtraFile = /#FROM_FILE#/;
+const UglifyJSOptions = {
+  module: false,
+  compress: {
+    expression: true,
+  },
+};
 
 // Define paths
 const __filename = fileURLToPath(import.meta.url);
@@ -25,10 +31,9 @@ basicMeta['lastUpdateTime'] = Date.now();
 
 // Add search scripts
 const searchScript = fs.readFileSync(resolve(__sourcedir, './searchUrl.js'), { encoding: 'utf-8' });
-const searchScriptMinify = UglifyJS.minify(searchScript);
-basicMeta['searchUrl'] = `@js:${searchScript}`;
-// if (!searchScriptMinify.error) basicMeta['searchUrl'] = `@js:${searchScriptMinify.code}`;
-// else basicMeta['searchUrl'] = `@js:${searchScript}`;
+const searchScriptMinify = UglifyJS.minify(searchScript, UglifyJSOptions);
+if (!searchScriptMinify.error) basicMeta['searchUrl'] = `@js:${searchScriptMinify.code}`;
+else basicMeta['searchUrl'] = `@js:${searchScript}`;
 
 // Read rule paths
 const ruleNames = (() => {
@@ -54,11 +59,10 @@ for (const ruleName of ruleNames) {
     if (RegExtraFile.test(ruleMeta[name])) {
       const extraFilePath = join(rulePath, `${name}.js`);
       const extraFile = fs.readFileSync(extraFilePath, { encoding: 'utf-8' });
-      const extraFileMinify = UglifyJS.minify(extraFile);
+      const extraFileMinify = UglifyJS.minify(extraFile, UglifyJSOptions);
 
-      ruleMeta[name] = ruleMeta[name].replace(RegExtraFile, extraFile);
-      // if (!extraFileMinify.error) ruleMeta[name] = ruleMeta[name].replace(RegExtraFile, extraFileMinify.code);
-      // else ruleMeta[name] = ruleMeta[name].replace(RegExtraFile, extraFile);
+      if (!extraFileMinify.error) ruleMeta[name] = ruleMeta[name].replace(RegExtraFile, extraFileMinify.code);
+      else ruleMeta[name] = ruleMeta[name].replace(RegExtraFile, extraFile);
     }
   }
   basicMeta[`rule${ruleName}`] = ruleMeta;
